98ead3a8fc3deffc7675884ff00ad2ce8cb7c976
LucFII <LSFIIChi@Gmail.com>
2017-05-23 17:13:25 -0500

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,30 @@
-$( document ).ready(function(){
-  var Random=Math.floor(Math.random()*101+19)
-  // Selects random number to be shown at the start of the game
-  // Number should be should be between 19 - 120
-  //
-  $('#randomNumber').text(Random);
-  // Appending random number to the randomNumber id in the html doc
-  //
-  var num1= Math.floor(Math.random()*11+1)
-  var num2= Math.floor(Math.random()*11+1)
-  var num3= Math.floor(Math.random()*11+1)
-  var num4= Math.floor(Math.random()*11+1)
-  // Setting up random numbers for each jewel
-  // Random number has to be between 1 - 12
-  // 
-  var userTotal= 0; 
-  var wins= 0;
-  var losses = 0;
-  //  Decaring variables for tallies
+// Selects random number between 19 and 120 at game start
+//$( document ).ready(function(){
+var Random=Math.floor(Math.random()*101+19)
+  
+
+// Adds random number to the randomNumber id in the html
+$('#randomNumber').text(Random);
+  
+
+// Sets up random numbers for each crystal between 1 and 12
+var num1= Math.floor(Math.random()*11+1)
+var num2= Math.floor(Math.random()*11+1)
+var num3= Math.floor(Math.random()*11+1)
+var num4= Math.floor(Math.random()*11+1)
+
+
+//  Tally Variables
+var userTotal= 0; 
+var wins= 0;
+var losses = 0;
+
+
 $('#numberWins').text(wins);
 $('#numberLosses').text(losses);
-//resets the game
+
+
+// Game reset
 function reset(){
       Random=Math.floor(Math.random()*101+19);
       console.log(Random)
@@ -30,27 +35,34 @@ function reset(){
       num4= Math.floor(Math.random()*11+1);
       userTotal= 0;
       $('#finalTotal').text(userTotal);
-      } 
-//adds the wins to the userTotal
+    } 
+
+
+// Adds wins to userTotal
 function win(){
 alert("You won!");
   wins++; 
   $('#numberWins').text(wins);
   reset();
 }
-//addes the losses to the userTotal
+
+
+// Adds loss to userTotal
 function lose(){
 alert ("Sorry, Try Again!");
   losses++;
   $('#numberLosses').text(losses);
   reset()
 }
-//sets up click for jewels
-  $('#one').on ('click', function(){
+
+
+// Click event for jewels
+  $('#one').on('click', function(){
     userTotal = userTotal + num1;
     console.log("New userTotal= " + userTotal);
     $('#finalTotal').text(userTotal); 
-          //sets win/lose conditions
+          
+// Win/lose conditions
         if (userTotal == Random){
           win();
         }
@@ -58,7 +70,8 @@ alert ("Sorry, Try Again!");
           lose();
         }   
   })  
-  $('#two').on ('click', function(){
+
+  $('#two').on('click', function(){
     userTotal = userTotal + num2;
     console.log("New userTotal= " + userTotal);
     $('#finalTotal').text(userTotal); 
@@ -68,20 +81,23 @@ alert ("Sorry, Try Again!");
         else if ( userTotal > Random){
           lose();
         } 
-  })  
-  $('#three').on ('click', function(){
+  }) 
+
+  $('#three').on('click', function(){
     userTotal = userTotal + num3;
     console.log("New userTotal= " + userTotal);
     $('#finalTotal').text(userTotal);
-//sets win/lose conditions
+
+// Win/lose conditions
           if (userTotal == Random){
           win();
         }
         else if ( userTotal > Random){
           lose();
         } 
-  })  
-  $('#four').on ('click', function(){
+  })
+
+  $('#four').on('click', function(){
     userTotal = userTotal + num4;
     console.log("New userTotal= " + userTotal);
     $('#finalTotal').text(userTotal); 
@@ -93,4 +109,4 @@ alert ("Sorry, Try Again!");
           lose();
         }
   });   
-}); 
+//}); 
