82aa7a4993735370bc1989e84b3e78c5c39ff526
LucFII <LSFIIChi@Gmail.com>
2017-05-23 23:17:56 -0500

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,9 @@
+// NOTES
+// When playing, you can surpass the target number without ending the game.
+  // Eventually, the game says you win/lose.
+// Also, you can lose before reaching the target number.
+
+
 // Selects random number between 19 and 120 at game start
 var targetRandom=Math.floor(Math.random()*101+19);
   
@@ -25,16 +31,16 @@ $('#numberLosses').html(losses);
 
 // Game reset
 function reset(){
-      random=Math.floor(Math.random()*101+19);
-      console.log(random)
-      $('#randomNumber').text(random);
-      num1= Math.floor(Math.random()*11+1);
-      num2= Math.floor(Math.random()*11+1);
-      num3= Math.floor(Math.random()*11+1);
-      num4= Math.floor(Math.random()*11+1);
-      userTotal= 0;
-      $('#finalTotal').text(userTotal);
-    } 
+  random=Math.floor(Math.random()*101+19);
+  console.log(random)
+  $('#randomNumber').text(random);
+  num1= Math.floor(Math.random()*11+1);
+  num2= Math.floor(Math.random()*11+1);
+  num3= Math.floor(Math.random()*11+1);
+  num4= Math.floor(Math.random()*11+1);
+  userTotal= 0;
+  $('#finalTotal').text(userTotal);
+} 
 
 
 // Adds wins to userTotal
@@ -62,24 +68,24 @@ alert ("Sorry, Try Again!");
     $('#finalTotal').html(userTotal); 
           
 // Win/lose conditions
-        if (userTotal == targetRandom){
-          win();
-        }
-        else if ( userTotal >targetRandom){
-          lose();
-        }   
+  if (userTotal == targetRandom){
+    win();
+  }
+  else if ( userTotal >targetRandom){
+    lose();
+  }   
   })  
 
   $('#two').on('click', function(){
     userTotal = userTotal + num2;
     console.log("New userTotal= " + userTotal);
     $('#finalTotal').html(userTotal); 
-        if (userTotal == targetRandom){
-          win();
-        }
-        else if ( userTotal > targetRandom){
-          lose();
-        } 
+      if (userTotal == targetRandom){
+        win();
+      }
+      else if ( userTotal > targetRandom){
+        lose();
+      } 
   }) 
 
   $('#three').on('click', function(){
@@ -88,23 +94,22 @@ alert ("Sorry, Try Again!");
     $('#finalTotal').html(userTotal);
 
 // Win/lose conditions
-          if (userTotal == targetRandom){
-          win();
-        }
-        else if ( userTotal > targetRandom){
-          lose();
-        } 
+  if (userTotal == targetRandom){
+    win();
+  }
+  else if ( userTotal > targetRandom){
+    lose();
+  } 
   })
 
   $('#four').on('click', function(){
     userTotal = userTotal + num4;
     console.log("New userTotal= " + userTotal);
     $('#finalTotal').text(userTotal); 
-      
-          if (userTotal == targetRandom){
-          win();
-        }
-        else if ( userTotal > targetRandom){
-          lose();
-        }
+      if (userTotal == targetRandom){
+        win();
+      }
+      else if ( userTotal > targetRandom){
+        lose();
+      }
   });
